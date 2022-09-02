14fda070bb213fe6418c3051fc04be5e3438802f
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-02 21:10:29 +0300

    add nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 
 const App = () => {
@@ -8,13 +9,7 @@ const App = () => {
     
     <div className='app-wrapper'>
     <Header/>
-     <nav className='nav'> 
-      <div> <a href="/#">Profile</a></div>
-      <div><a href="/#">Messages</a></div>
-      <div> <a href="/#">News</a></div>
-      <div> <a href="/#">Music</a></div>
-      <div> <a href="/#">Setting</a></div>
-     </nav>
+      <Navbar />
       <div className='content'>
       <img src="https://cutewallpaper.org/21/linkedin-background-images/chicago-skyline-linkedIn-background-Blackmore-Partners-Inc.jpg" alt="bg" />
       <div>My post</div>
