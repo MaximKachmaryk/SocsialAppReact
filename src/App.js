d47dfc762627b427a5ed0900296fd4c8e2b8506d
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-02 21:05:31 +0300

    add files with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div className='app-wrapper'>
+    <Header/>
+     <nav className='nav'> 
+      <div> <a href="/#">Profile</a></div>
+      <div><a href="/#">Messages</a></div>
+      <div> <a href="/#">News</a></div>
+      <div> <a href="/#">Music</a></div>
+      <div> <a href="/#">Setting</a></div>
+     </nav>
+      <div className='content'>
+      <img src="https://cutewallpaper.org/21/linkedin-background-images/chicago-skyline-linkedIn-background-Blackmore-Partners-Inc.jpg" alt="bg" />
+      <div>My post</div>
+      <div>New post</div>
+      <div>post 1</div>
+      <div>post 1</div></div>
     </div>
-  );
+
+  )
 }
+
 
 export default App;
