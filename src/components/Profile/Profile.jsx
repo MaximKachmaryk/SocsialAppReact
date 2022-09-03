025f79f33061bacff7747e9ac824ac0af1c3ea69
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-03 17:56:17 +0300

    add description

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import MyPosts from './Myposts/Myposts';
+import MyPost from './Myposts/Myposts';
 import classes from './Profile.module.css'
 
 const Profile = () =>{
     return  <div className={classes.content}>
     <img src="https://cutewallpaper.org/21/linkedin-background-images/chicago-skyline-linkedIn-background-Blackmore-Partners-Inc.jpg" alt="bg" />
-   
-    <MyPosts/></div>
+   <div>ava+description</div>
+   <textarea ></textarea>
+   <button></button>
+    <MyPost/>
+    </div>
     
 }
 export default Profile;                              
