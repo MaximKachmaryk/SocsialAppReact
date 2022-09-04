74204e21c8f06ec62169eb03b540be1b4c6a8925
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-04 23:43:57 +0300

    add dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,27 @@
 import React from "react";
-/* import classes from './Dialogs.module.css' */
+import classes from "./Dialogs.module.css";
+
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={classes.dialog + " " + classes.active}>Maxim </div>
+        <div className={classes.dialog}>Vasyl </div>
+        <div className={classes.dialog}>Stepn </div>
+        <div className={classes.dialog}>Ignat </div>
+        <div className={classes.dialog}>Petro </div>
+        <div className={classes.dialog}>Maxim </div>
+        <div className={classes.dialog}>Vasyl </div>
+        <div className={classes.dialog}>Stepn </div>
+        <div className={classes.dialog}>Ignat </div>
+        <div className={classes.dialog}>Petro </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>Hi</div>
+        <div className={classes.message}>How is you IT</div>
+        <div className={classes.message}>YO</div>
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,8 +9,8 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}> <NavLink to ="/profile">Profile</NavLink> </div>
-            <div className={classes.item}><NavLink to="/messages">Messages</NavLink></div>
+            <div className={classes.item}> <NavLink to ="/profile" activeClassName={classes.activeLink}>Profile</NavLink> </div>
+            <div className={classes.item}><NavLink to="/dialogs">Messages</NavLink></div>
             <div className={classes.item}> <NavLink to="/news">News</NavLink></div>
             <div className={classes.item}> <NavLink to="/music">Music</NavLink></div>
             <div className={classes.item}> <NavLink to="/setting">Setting</NavLink></div>
