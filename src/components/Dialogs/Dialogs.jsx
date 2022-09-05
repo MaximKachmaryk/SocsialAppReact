7b0b1894ba398f22b1b55372cb383231e15c8ed2
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-05 15:02:16 +0300

    add arrays

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,20 +12,32 @@ const Message = (props) => {
   return <div className={classes.message}>{props.message}</div>;
 };
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: 1, name: "Maxym" },
+    { id: 2, name: "Roman" },
+    { id: 3, name: "Petro" },
+    { id: 4, name: "Vlad" },
+    { id: 5, name: "Stepan" },
+    { id: 6, name: "Volodymyr" }
+  ]
+  let messeges=[
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How is you IT" },
+    { id: 3, message: "YO" }
+  ]
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <DialogItem name="Maxym" id="1" />
-        <DialogItem name="Roman" id="2" />
-        <DialogItem name="Petro" id="3" />
-        <DialogItem name="Vlad" id="4" />
-        <DialogItem name="Stepan" id="5" />
-        <DialogItem name="Volodymyr" id="6" />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        
       </div>
       <div className={classes.messages}>
-        <Message message="Hi" />
-        <Message message="How is you IT" />
-        <Message message="YO" />
+        <Message message={messeges[0].message} />
+        <Message message={messeges[1].message} />
+        <Message message={messeges[2].message} />
       </div>
     </div>
   );
--- src/components/Profile/Myposts/Myposts.jsx
+++ src/components/Profile/Myposts/Myposts.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import classes from "./Myposts.module.css";
 import Post from "./Post/Post";
 const MyPosts = (props) => {
+  let postData=[
+    
+    { id: 1, message: "How is you IT",likesCoutn:12 },
+    { id: 3, message: "My first post",likesCoutn:12 }]
   return (
+    
     <div className={classes.postsBlock}>
       <h3>My Pots</h3>
       <div><textarea></textarea></div>
       <div><button></button></div>
-      <Post massage="how are you" likesCount="0" />
-      <Post massage="i am ok" />
+      <Post massage={postData[0].message} likesCount={postData[0].likesCoutn} />
+      <Post massage={postData[1].message} likesCount={postData[1].likesCoutn} />
       
       
       
